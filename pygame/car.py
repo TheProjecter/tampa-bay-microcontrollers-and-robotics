744f790b235b1154d981836133fac0ef4bffb8ce
Bruce Frederiksen <dangyogi@gmail.com>
2010-04-02 18:59:12 -0400

    I think rotate works now

--- pygame/car.py
+++ pygame/car.py
@@ -1,7 +1,10 @@
 # car.py
 
+from __future__ import division
+
 import sys
 import time
+import math
 import pygame
 
 Size = Width, Height = 640, 480
@@ -17,7 +20,7 @@ Black = pygame.Color(0, 0, 0)
 Car_body = (3, 2, 10, 20)
 
 Car_components = (
-    (LtBlue, Car_body),       # main body
+    (Blue, Car_body),         # main body
     (Black, (0, 0, 3, 6)),    # lf wheel
     (Black, (13, 0, 3, 6)),   # rf wheel
     (Black, (0, 18, 3, 6)),   # lr wheel
@@ -27,13 +30,24 @@ Car_components = (
 class car_image(pygame.Surface):
     def __init__(self):
         super(car_image, self).__init__((16, 24))
+        self.set_colorkey(White, pygame.RLEACCEL)
         self.fill(White)
         for color, rect in Car_components:
             self.fill(color, rect)
+        self.logical_center = (0, -10)   # from image center to logical center
 
 class rotatable_sprite(pygame.sprite.Sprite):
-    def __init__(self, group):
+    def __init__(self, group, image):
         super(rotatable_sprite, self).__init__(group)
+        self.base_image = image
+        self.heading = 0.0
+        self.image = image
+        self.rect = self.image.get_rect()
+
+        # rect.topleft + offset == logical image center
+        cx, cy = self.rect.center
+        lcx, lcy = self.image.logical_center
+        self.offset = cx + lcx, cy + lcy
 
     def update(self, fn, *args):
         getattr(self, fn)(*args)
@@ -42,13 +56,34 @@ class rotatable_sprite(pygame.sprite.Sprite):
         self.rect = self.rect.move(dx, dy)
         return self.rect
 
+    def rotate(self, angle):
+        x, y = self.offset
+        logical_centerx = self.rect.left + x
+        logical_centery = self.rect.top + y
+        print "pre-rotated rect", self.rect, "at heading", self.heading
+        print "logical_centerx", logical_centerx, "logical_centery", logical_centery
 
-class car(rotatable_sprite):
-    def __init__(self):
-        super(car, self).__init__(Robot)
-        self.image = car_image()
+        self.heading += angle
+        self.image = pygame.transform.rotate(self.base_image, -self.heading)
         self.rect = self.image.get_rect()
-        #print 'car rect', self.rect
+        ra = math.radians(self.heading)
+        sin, cos = math.sin(ra), math.cos(ra)
+        x, y = self.base_image.logical_center
+        cx, cy = self.rect.center
+        lcx = x * cos - y * sin
+        lcy = y * cos + x * sin
+        ox = cx + lcx
+        oy = cy + lcy
+        self.offset = ox, oy
+        print "new self.offset", self.offset
+        self.rect.left = logical_centerx - ox
+        self.rect.top = logical_centery - oy
+        print "rotated rect", self.rect, "at heading", self.heading
+
+
+class robot_sprite(rotatable_sprite):
+    def __init__(self, image):
+        super(robot_sprite, self).__init__(Robot, image)
 
 Range_width, Range_distance = 20, 96
 
@@ -58,6 +93,9 @@ class range_finder_image(pygame.Surface):
         self.set_colorkey(White, pygame.RLEACCEL)
         self.fill(Red)
         self.fill(White, (2, 2, Range_width - 4, Range_distance - 4))
+
+        # from image center to logical center
+        self.logical_center = 0, Range_distance/2
 
 class robot(pygame.sprite.RenderUpdates):
     def __init__(self):
@@ -73,12 +111,18 @@ class robot(pygame.sprite.RenderUpdates):
         return ans
 
 Robot = robot()
-car()
+Car = robot_sprite(car_image())
+Car.update('rotate', 30.0)
 
 class background(pygame.Surface):
     def __init__(self):
         super(background, self).__init__((Width, Height))
         self.fill(White)
+
+        self.mask = pygame.Mask((Width, Height))
+        for x in range(Width):
+            for y in range(Height):
+                self.mask.set_at((x, y), 0)
 
 Background = background()
 
@@ -89,9 +133,15 @@ class obstacle(pygame.sprite.Sprite):
         self.image = pygame.Surface((width, height))
         self.image.fill(Black)
         Background.blit(self.image, self.rect)
+        self.mask = pygame.mask.from_surface(self.image)
 
 Obstacles = pygame.sprite.Group()
-#obstacle((Width - 150)//2, (Height - 200)//2, 150, 200)
+Building = obstacle((Width - 150)//2, (Height - 200)//2, 150, 200)
+
+def test():
+    print "collide", pygame.sprite.collide_mask(Car, Building)
+    Robot.move(((Width - 150)//2, (Height - 200)//2))
+    print "collide", pygame.sprite.collide_mask(Car, Building)
 
 def init():
     global Screen
