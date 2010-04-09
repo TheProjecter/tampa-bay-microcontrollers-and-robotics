0780aa9789a90575ca43ceb3eb9ce92327403418
Bruce Frederiksen <dangyogi@gmail.com>
2010-04-09 16:00:01 -0400

    It works!
    But doesn't have obstacle avoidance yet...

--- driver/driver.py
+++ driver/driver.py
@@ -1,15 +1,19 @@
 # driver.py
 
+import math
 import sys
 import itertools
-from pyke import knowledge_engine, krb_traceback
 
-import robot_simulator
-
-Engine = knowledge_engine.engine(__file__)
+if __name__ == "__main__":
+    import doctest
+    sys.exit(doctest.testmod()[0])
+else:
+    from pyke import knowledge_engine, krb_traceback
+    import robot_simulator
+    Engine = knowledge_engine.engine(__file__)
 
 def run():
-    global Robot
+    global Robot, Building_center
 
     Robot = robot_simulator.start()
 
@@ -17,12 +21,14 @@ def run():
     #
     #   layout.board(width, height)
     #   layout.building(left, top, right, bottom)
+    #   layout.building_center(x, y)
     #   layout.car(width, height)
     #   layout.car_logical_center(x_offset, y_offset)
     #   layout.range_finder(width, length)
     #   layout.safety_factor(safety_factor)
     #   layout.track(left, top, right, bottom)
     #   layout.direction(cc|ccw)
+    #   layout.waypoint(x, y)
 
     Engine.add_universal_fact('layout', 'board',
                               (robot_simulator.Width, robot_simulator.Height))
@@ -34,6 +40,9 @@ def run():
     print "building corners", corners
     Engine.add_universal_fact('layout', 'building',
                               corners)
+    Building_center = robot_simulator.Building.center
+    Engine.add_universal_fact('layout', 'building_center',
+                              Building_center)
     Engine.add_universal_fact('layout', 'car',
                               Robot.car.rect[2:])
     Engine.add_universal_fact('layout', 'car_logical_center',
@@ -49,20 +58,14 @@ def run():
              bottom + safety_factor)
     print "track", track
     Engine.add_universal_fact('layout', 'track', track)
-    x, y = Robot.position
-    heading = Robot.heading % 360
-    if x <= left:
-        direction = 'ccw' if 90 <= heading <= 270 else 'cw'
-    elif x >= right:
-        direction = 'cw' if 90 <= heading <= 270 else 'ccw'
-    elif y <= top:
-        direction = 'cw' if heading <= 180 else 'ccw'
-    elif y >= bottom:
-        direction = 'ccw' if heading <= 180 else 'cw'
-    else:
-        raise AssertionError("car starting inside building!")
-    print "direction", direction
-    Engine.add_universal_fact('layout', 'direction', (direction,))
+    initial_direction = direction(Robot.heading)
+    print "initial_direction", initial_direction
+    Engine.add_universal_fact('layout', 'direction', (initial_direction,))
+    Engine.add_universal_fact('layout', 'waypoint', (track[0], track[1]))
+    Engine.add_universal_fact('layout', 'waypoint', (track[0], track[3]))
+    Engine.add_universal_fact('layout', 'waypoint', (track[2], track[1]))
+    Engine.add_universal_fact('layout', 'waypoint', (track[2], track[3]))
+    Engine.add_universal_fact('layout', 'waypoint', Robot.position)
 
     for step in itertools.count():
         x, y = Robot.position
@@ -70,19 +73,51 @@ def run():
         Engine.add_universal_fact('history', 'position', (step, x, y))
         print "Robot heading %d" % Robot.heading
         Engine.add_universal_fact('history', 'heading', (step, Robot.heading))
+        Engine.add_universal_fact('history', 'car_radial',
+                                  (step,
+                                   heading(Building_center, Robot.position)))
         Engine.reset()
         Engine.activate('rules')
         print "proving step", step
         try:
-            vars, plan = Engine.prove_1_goal('rules.get_plan($step, $done)',
-                                             step=step)
+            vars, _ = Engine.prove_1_goal('rules.get_plan($step, $plan)',
+                                          step=step)
         except:
             krb_traceback.print_exc()
             sys.exit(1)
-        if vars['done']:
+        plan = vars['plan']
+        if plan is None:
             print "Success!"
             break
         ans = plan()
         print "plan done, Robot at (%d, %d)" % Robot.position
         Engine.add_universal_fact('history', 'result', (step, ans))
 
+def heading(from_pos, to_pos):
+    r'''Returns heading in degrees from from_pos to to_pos.
+
+    heading is an int between 0 and 359, with 0 up.
+
+        >>> heading((100, 100), (100, 50))
+        0
+        >>> heading((100, 100), (100, 150))
+        180
+        >>> heading((100, 100), (50, 100))
+        270
+        >>> heading((100, 100), (150, 100))
+        90
+    '''
+    fx, fy = from_pos
+    tx, ty = to_pos
+    return int(round(math.degrees(math.atan2(tx - fx, fy - ty)) % 360))
+
+def direction(car_heading):
+    car_radial = heading(Building_center, Robot.position)
+    if (car_radial - car_heading) % 360 <= 180:
+        return 'ccw'
+    return 'cw'
+
+def distance(from_pos, to_pos):
+    fx, fy = from_pos
+    tx, ty = to_pos
+    return math.hypot(tx - fx, ty - fy)
